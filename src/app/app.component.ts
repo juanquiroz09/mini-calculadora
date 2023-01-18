22fc1ffeeab85e48e1da3309580323c65c80ccd3
Juan Quiroz <juan.quiroz1009@gmail.com>
2023-01-18 16:49:52 -0500

    calculadora-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mini-calculadora';
+  title = 'Mini Calculadora';
+  number1 : number = 0;
+  number2 : number = 0;
+  result : number = 0;
+
+  sumar(event: Event):void {
+    this.result = this.number1 + this.number2;
+  }
+  restar(event: Event):void {
+    this.result = this.number1 - this.number2;
+  }
 }
